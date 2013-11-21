b89972e15c69f12f99ee86a2e37d65e56be034f3
unknown <chethana@INchethana.corporate.thoughtworks.com>
2013-11-21 16:35:34 +0530

    [Grunt] [Prince, Chethan] Changed the grunt file according to the folder structure

--- gruntfile.js
+++ gruntfile.js
@@ -1,10 +1,5 @@
 module.exports = function(grunt) {
-  var target = grunt.option('target');
-  var target_js = target || '*.js';
-  var target_css = target || '*.css';
-
-  var js_dest = target_js.replace(".js","");
-  js_dest = js_dest.replace("*","uglified");
+  var foldername = grunt.option('foldername');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -13,58 +8,59 @@ module.exports = function(grunt) {
       options: {
         banner: '/*! <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      js: {
-        src: 'src/js/'+ target_js,
-        dest: 'build/js/'+js_dest+'.min.js'
+      js_min: {
+        src: 'lib/components/'+foldername+'/*.js',
+        dest: 'target/lib/components/'+foldername+'/'+foldername+'.min.js'
       }
     }
     ,
-    cssmin:{
-      combine:{
-        files:{
-        'build/css/uglified.min.css':['src/css/*.css'] 
-        }  
-      }
+    cssmin: {
+      minify: {
+        expand: true,
+        src: ['lib/components/'+foldername+'/*.css'],
+        dest: 'target/',
+        ext: '.min.css'
     }
+  }
     ,
     concat:{
       options: {
         banner: '/*! singlified <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
        js: {
-        src:'src/js/*.js',
-        dest:'build/js/singlified.js'
+        src:'lib/components/**/*.js',
+        dest:'target/lib/components/ikebana/js-ikebana.js'
       },
       css: {
-        src: 'src/css/*.css',
-        dest: 'build/css/singlified.css'
+        src: 'lib/components/**/*.css',
+        dest: 'target/lib/components/ikebana/css-ikebana.css'
       }
     }
-    ,
-    compress:{
-      main:{
-      options:{
-        archive:'compressed/archived.zip'
-      },
-      files:[
-        {src:['src/js/*.js','src/css/*.css']},
-      ]
-    }
-    }
+    // ,
+    // compress:{
+    //   main:{
+    //   options:{
+    //     archive:'compressed/archived.zip'
+    //   },
+    //   files:[
+    //     {src:['src/js/*.js','src/css/*.css']},
+    //   ]
+    // }
+    // }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-compress');
+  // grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('minify', ['uglify:js','cssmin']);
-  grunt.registerTask('minify-js', ['uglify:js']);
+  grunt.registerTask('minify', ['uglify:js_min','cssmin']);
+  grunt.registerTask('minify-js', ['uglify:js_min']);
   grunt.registerTask('minify-css', ['cssmin']);
 
   grunt.registerTask('singlify',['concat:js','concat:css']);
   grunt.registerTask('singlify-js',['concat:js']);
   grunt.registerTask('singlify-css',['concat:css']);
   
-  grunt.registerTask('archive',['compress']);
+  // grunt.registerTask('archive',['compress']);
 };
