6d31f5589fc14548e352042f50274d28763a1b36
Pranav Kudva Vishwanath <pranavkv@thoughtworks.com>
2013-11-22 20:29:14 +0530

    [Build] [Chethan, Pranav] Archiving done. Added archiving to build script. Also renamed few variables

--- gruntfile.js
+++ gruntfile.js
@@ -1,81 +1,88 @@
 module.exports = function(grunt) {
-  var foldername = grunt.option('foldername');
+  var component = grunt.option('component');
+  var archive = typeof component != 'undefined' ? component : 'ikebana';
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+      pkg: grunt.file.readJSON('package.json'),
 
-    clean: ["target"],
+      clean: ["target"],
 
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      js_selected: {
-        src: 'lib/components/'+foldername+'/*.js',
-        dest: 'target/lib/components/'+foldername+'/'+foldername+'.min.js'
-      },
-      js_all:{
-        src: 'target/lib/components/ikebana/js-ikebana.js',
-        dest: 'target/lib/components/ikebana/ikebana.min.js'
+      uglify: {
+        options: {
+          banner: '/*! <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        selected_component_js: {
+          src: 'lib/components/'+component+'/*.js',
+          dest: 'target/lib/components/'+component+'/'+component+'.min.js'
+        },
+        all_component_js:{
+          src: 'target/lib/components/ikebana/ikebana.js',
+          dest: 'target/lib/components/ikebana/ikebana.min.js'
+        }
       }
-    }
-    ,
-    cssmin: {
-      selected: {
-        expand: true,
-        src: ['lib/components/'+foldername+'/*.css'],
-        dest: 'target/',
-        ext: '.min.css'
-      },
-      all: {
+      ,
+      cssmin: {
+        selected_component: {
           expand: true,
-          src: ['target/lib/components/ikebana/*.css'],
+          src: ['lib/components/'+component+'/*.css'],
+          dest: 'target/',
           ext: '.min.css'
+        },
+        all_component: {
+            expand: true,
+            src: ['target/lib/components/ikebana/*.css'],
+            ext: '.min.css'
+        }
+      }
+      ,
+      concat:{
+        options: {
+          banner: '/*! singlified <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        selected_component_js: {
+          src:'lib/components/'+component+'/*.js',
+          dest:'target/lib/components/'+component+'/'+component+'-singlified.js'
+        },
+        selected_component_css: {
+          src: 'lib/components/'+component+'/*.css',
+          dest: 'target/lib/components/'+component+'/'+component+'-singlified.css'
+        }, 
+        all_component_js: {
+          src:'lib/components/**/*.js',
+          dest:'target/lib/components/ikebana/ikebana.js'
+        },
+        all_component_css: {
+          src: 'lib/components/**/*.css',
+          dest: 'target/lib/components/ikebana/ikebana.css'
+        }
+      }
+      ,
+      compress:{
+        main : {
+          options: {
+              mode: 'zip',
+              archive : 'target/compressed/'+ archive +'.zip'
+          },
+
+          files: [
+            {expand: true, cwd: 'target/lib/components/'+ archive, src: ['*']}
+          ]
+        }
       }
     }
-    ,
-    concat:{
-      options: {
-        banner: '/*! singlified <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      js_selected: {
-        src:'lib/components/'+foldername+'/*.js',
-        dest:'target/lib/components/'+foldername+'/'+foldername+'-singlified.js'
-      },
-      css_selected: {
-        src: 'lib/components/'+foldername+'/*.css',
-        dest: 'target/lib/components/'+foldername+'/'+foldername+'-singlified.css'
-      }, 
-      js_all: {
-        src:'lib/components/**/*.js',
-        dest:'target/lib/components/ikebana/js-ikebana.js'
-      },
-      css_all: {
-        src: 'lib/components/**/*.css',
-        dest: 'target/lib/components/ikebana/css-ikebana.css'
-      }
-    }
-    // ,
-    // compress:{
-    //   main:{
-    //   options:{
-    //     archive:'compressed/archived.zip'
-    //   },
-    //   files:[
-    //     {src:['src/js/*.js','src/css/*.css']},
-    //   ]
-    // }
-    // }
-  });
+  );
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-   grunt.loadNpmTasks('grunt-contrib-clean');
-  // grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.registerTask('clear',['clean']);
-  grunt.registerTask('build-all',['concat:js_all','concat:css_all','uglify:js_all','cssmin:all']);
-  grunt.registerTask('build-selected',['concat:js_selected','concat:css_selected','uglify:js_selected','cssmin:selected']);
 
-  // grunt.registerTask('archive',['compress']);
+  grunt.registerTask('clear',['clean']);
+  
+  grunt.registerTask('build-ikebana',['concat:all_component_js','concat:all_component_css','uglify:all_component_js','cssmin:all_component']);
+  grunt.registerTask('build-this',['concat:selected_component_js','concat:selected_component_css','uglify:selected_component_js','cssmin:selected_component']);
+
+  grunt.registerTask('archive-all',['compress']);
+  grunt.registerTask('archive-this',['compress']);
 };
