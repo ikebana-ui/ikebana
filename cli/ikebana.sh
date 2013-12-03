9124783c8456c37484954a8b5521efd92016d0ec
unknown <chethana@INchethana.corporate.thoughtworks.com>
2013-12-03 19:14:47 +0530

    [ikebana-cli][Chethan, Pranav] Made changes to the cli file to give messages to user

--- cli/ikebana.sh
+++ cli/ikebana.sh
@@ -1,9 +1,9 @@
-if [[ $1 -eq "" ]]; then
+if [[ $1 == "" ]]; then
 	flavour_component="ikebana"
 else
 	flavour=$(echo $1|cut -f1 -d':')
 	component=$(echo $1|cut -f2 -d':')
-	if [ $component -eq "all" -o $component -eq $flavour]; then
+	if [[ $component == "all" || $component == $flavour ]]; then
 		flavour_component=$flavour
 	else flavour_component=$flavour'_'$component
 	fi
@@ -14,8 +14,9 @@ response=$(curl --write-out %{http_code} --silent --output temp.zip localhost:80
 if [[ $response -eq 200 ]]; then
 	unzip -q -d $flavour_component/ temp.zip 
 	rm -rf temp.zip
+	echo "successfully installed $flavour_component."
 elif [[ $response -eq 404 ]]; then
-	echo "$1 not found"
+	echo "$1 not found."
 	echo "SYNTAX: ikebana <flavour_name>:<component_name>"
 	echo -e "\t\tOR"
 	echo -e "\tikebana <flavour_name>:all"
