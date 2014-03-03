b29ce076e10caadd2bf0b0990f98de94c515d69c
Rishabh Rao <rishabhsrao@gmail.com>
2014-03-04 04:24:57 +0530

    :wrench: Added a jasmine sample test suite.

---
+++ template/component/spec/componentSpec.js
@@ -0,0 +1,12 @@
+/**
+  * Test spec for <%= name %> - <%= pkg.name %>
+  * @version v<%= pkg.version %>
+  * @link <%= pkg.url %>
+  * @license <%= pkg.license %>
+  */
+
+describe("Test suite for <%= name %>", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
